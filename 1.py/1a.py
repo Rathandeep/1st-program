f31f2987cfef252b881536b9305355901868fef4
Rathandeep <deepcsrathandeep@gmail.com>
2024-05-23 21:12:25 +0530

    Add files via upload

---
+++ 1.py/1a.py
@@ -0,0 +1,12 @@
+ 
+num = int(input("Enter the integer value: ")) 
+amt = float(input("Enter the float value: ")) 
+code = str(input("Enter the character value: ")) 
+msg = str(input("Enter the string value: ")) 
+comp = complex(input("Enter the complex value:")) 
+boolean = bool(input("Enter the boolean value:")) 
+nonetype = None 
+#Print the values of variables 
+print("Entered integer value is =",num,"\nEntered float value is =",amt,"\nEntered character value is 
+=",code,"\nEntered string value is =",msg, "\nEntered complex value is =",comp, "\nEntered boolean 
+value is =",boolean, "\nEntered NoneType value is =",nonetype)
